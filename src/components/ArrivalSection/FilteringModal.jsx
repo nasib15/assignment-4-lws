afcb5c1b99dc7c46482475afbdd1117f05f3b77a
nasib15 <nasibhossain57@gmail.com>
2024-11-02 00:24:35 +0600

    Add: Category data from API

--- src/components/ArrivalSection/FilteringModal.jsx
+++ src/components/ArrivalSection/FilteringModal.jsx
@@ -1,19 +1,28 @@
+import { useFashionData } from "../../hooks/useFashionData";
+
 const FilteringModal = () => {
+  const { fashionData: categories, loading } = useFashionData(
+    "https://fakestoreapi.com/products/categories"
+  );
+
   return (
     <div className="absolute z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
       <div className="py-1" role="none">
-        <label className="inline-flex w-full cursor-pointer hover:bg-gray-50 items-center px-4 py-2 text-sm text-gray-700">
-          <input type="checkbox" className="form-checkbox h-4 w-4" />
-          <span className="ml-2">Category 1</span>
-        </label>
-        <label className="inline-flex w-full cursor-pointer hover:bg-gray-50 items-center px-4 py-2 text-sm text-gray-700">
-          <input type="checkbox" className="form-checkbox h-4 w-4" />
-          <span className="ml-2">Category 2</span>
-        </label>
-        <label className="inline-flex w-full cursor-pointer hover:bg-gray-50 items-center px-4 py-2 text-sm text-gray-700">
-          <input type="checkbox" className="form-checkbox h-4 w-4" />
-          <span className="ml-2">Category 3</span>
-        </label>
+        {loading ? (
+          <p className="inline-flex w-full cursor-pointer hover:bg-gray-50 items-center px-4 py-2 text-sm text-gray-700">
+            Loading....
+          </p>
+        ) : (
+          categories?.map((category, index) => (
+            <label
+              className="inline-flex w-full cursor-pointer hover:bg-gray-50 items-center px-4 py-2 text-sm text-gray-700"
+              key={index}
+            >
+              <input type="checkbox" className="form-checkbox h-4 w-4" />
+              <span className="ml-2">{category}</span>
+            </label>
+          ))
+        )}
       </div>
     </div>
   );
