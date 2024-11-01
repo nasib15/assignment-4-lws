c9edc9afd37bd699ace86b0f69a7aa98024ce754
nasib15 <nasibhossain57@gmail.com>
2024-11-01 19:16:12 +0600

    Project initialize

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-undef */
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      container: {
+        center: true,
+      },
+      colors: {
+        clifford: "#da373d",
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/aspect-ratio")],
+};
