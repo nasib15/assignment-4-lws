86662f2687f3b531bc8a3519b1d6e41e4dd57ed7
nasib15 <nasibhossain57@gmail.com>
2024-11-04 23:06:45 +0600

    Fix: Card height

--- src/components/ArrivalSection/ArrivalCard.jsx
+++ src/components/ArrivalSection/ArrivalCard.jsx
@@ -5,39 +5,37 @@ const ArrivalCard = ({ product, onCart, cartsData }) => {
   const isAddToCart = cartsData.find((item) => item.id === id);
 
   return (
-    <>
-      <div className="relative">
-        <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md lg:aspect-none lg:h-80">
-          <img
-            src={image}
-            alt={title}
-            className="h-full w-full object-cover object-top lg:h-full lg:w-full p-4 bg-gray-100"
-          />
+    <div className="relative h-full flex flex-col">
+      <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md lg:aspect-none lg:h-80">
+        <img
+          src={image}
+          alt={title}
+          className="h-full w-full object-cover object-top lg:h-full lg:w-full p-4 bg-gray-100"
+        />
+      </div>
+      <div className="mt-4 px-3">
+        <div>
+          <h3 className="text-sm text-gray-700">{title}</h3>
+          <p className="mt-1 text-sm text-gray-500">{category}</p>
         </div>
-        <div className="mt-4 px-3 pb-4">
-          <div>
-            <h3 className="text-sm text-gray-700">{title}</h3>
-            <p className="mt-1 text-sm text-gray-500">{category}</p>
-          </div>
-          <p className="text-sm font-medium text-gray-900">${price}</p>
-        </div>
-        <div className="cursor-pointer rounded-md bg-white text-[0.8125rem] font-medium leading-5 text-slate-700 ring-1 ring-slate-700/10  hover:ring-1 hover:bg-slate-50 hover:text-slate-900 items-center text-center mb-3 mx-3 flex-1">
+        <p className="text-sm font-medium text-gray-900 my-3">${price}</p>
+      </div>
+      <div className="mt-auto px-3 pb-3">
+        <div className="cursor-pointer rounded-md bg-white text-[0.8125rem] font-medium leading-5 text-slate-700 ring-1 ring-slate-700/10 hover:ring-1 hover:bg-slate-50 hover:text-slate-900">
           <div
             className={`flex px-3 py-2 justify-center ${
               isAddToCart
-                ? " bg-red-600 hover:bg-red-700 text-white rounded-md"
+                ? "bg-red-600 hover:bg-red-700 text-white rounded-md"
                 : ""
             }`}
-            onClick={() => {
-              onCart(id);
-            }}
+            onClick={() => onCart(id)}
           >
             <ShopIcon />
             {isAddToCart ? "Remove from Cart" : "Add to Cart"}
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 export default ArrivalCard;
--- src/components/ArrivalSection/ArrivalSection.jsx
+++ src/components/ArrivalSection/ArrivalSection.jsx
@@ -89,7 +89,7 @@ const ArrivalSection = () => {
         <div>
           <div className="bg-white">
             <div className="mx-auto max-w-2xl px-4 py-4 sm:px-6 lg:max-w-7xl lg:px-8">
-              <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+              <div className="mt-6 grid gri grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
                 {/* Showing loading splash screen till fetching data */}
                 {loading || loadingCategory ? (
                   <>
